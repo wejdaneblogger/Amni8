e2988eee95ff68e1f277d04c0a714a9bc906ce4d
skimi01 <skimi01@yopmail.org>
2018-02-21 17:43:54 +0200

    Update chat-head.js

--- chat-head.js
+++ chat-head.js
@@ -18,7 +18,7 @@ if (x_n == 0) {
 };
 var x_ss = document['getElementsByTagName']('script');
 for (i = 0; i < x_ss['length']; i++) {
-    if (x_ss[i]['src'] == 'http://designer4.me/peter/chat-head.js') {
+    if (x_ss[i]['src'] == 'https://cdn.rawgit.com/skimi01/Amni8/master/chat-head.js') {
         x_ss[i]['replaceWith']('')
     }
 }
